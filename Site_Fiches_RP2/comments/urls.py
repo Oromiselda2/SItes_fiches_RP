353eabc357f4ed3bfda48daa68dd318207524e24
Oromis <adresserptoutypes@gmail.com>
2026-02-20 13:04:35 +0100

    Delete Site_Fiches_RP2 directory

--- Site_Fiches_RP2/comments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('fiche/<int:fiche_pk>/add/', views.comment_create, name='comment_create'),
-    path('<int:pk>/edit/', views.comment_edit, name='comment_edit'),
-    path('<int:pk>/delete/', views.comment_delete, name='comment_delete'),
-]
--- Site_Fiches_RP2/core/context_processors.py
+++
@@ -1,9 +0,0 @@
-from fiches.models import Server
-
-
-def global_context(request):
-    """Context processor pour rendre certaines variables disponibles partout"""
-    context = {
-        'all_servers': Server.objects.all().order_by('name') if request.user.is_authenticated else [],
-    }
-    return context
